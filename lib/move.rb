cc1d982a0c9b34c0a46cd6d9c915b1dea6b72973
abstract-metric-6683 <rsneal01@gmail.com>
2020-04-05 20:29:06 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 
-board = [" ", " ", " ", " ", " ", " "" ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def move(board, position, char = "X")
   board[position] = char
